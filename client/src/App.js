76869c1a782e5ca2c9288dadd7c2b08d7c280f82
daudirfan <daudirfan@protonmail.com>
2019-04-14 00:40:13 +0500

    Final Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,61 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class App extends Component {
+
+  state = {
+    data: [],
+    id: 0,
+    message: null,
+    intervalIsSet: false,
+    idToDelete: null,
+    idToUpdate: null,
+    objectToUpdate: null
+  }
+  componentDidMount(){
+    this.getDataFromDB();
+    if(!this.state.intervalIsSet){
+      let interval = setInterval(this.getDataFromDB, 1000);
+      this.setState({ intervalIsSet: interval });
+    }
+  }
+  getDataFromDB = () => {
+    fetch("http://localhost:3001/api/getData").then(data => data.json()).then(res => this.setState({ data: res.data }));
+
+  };
+  putDataToDB = message => {
+    let currentIDs = this.state.data.map(data => data.id);
+    let idToBeAdded = 0;
+    while(currentIDs.includes(idToBeAdded)){
+      idToBeAdded++;
+    }
+
+    Axios.post("http://localhost:3001/api/putData", {id: idToBeAdded, message: message});
+
+  };
   render() {
+    const {data} = this.state;
     return (
-      <div>Ready to use react!</div>
+      <div>
+      <ul>
+        {
+          data.length <= 0
+          ? "NO DB ENTRIES YET"
+          : data.map( dat => (
+              <li>{dat.message}</li>
+            ))
+        }
+      </ul>
+      
+      <input
+        type="text"
+        onChange={e => this.setState({message: e.target.value})}
+        placeholder="Add message here....."
+      />
+      <button
+        onClick={() => this.putDataToDB(this.state.message)}
+      > ADD </button>
+      </div>
     );
   }
 }
